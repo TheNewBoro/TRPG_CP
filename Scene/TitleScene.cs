afd90a2ed63e3e1a5a830060d2b95bc76f146e5a
TheNewBoro <potar1132@gmail.com>
2025-04-10 12:47:50 +0900

    TitleScene

---
+++ Scene/TitleScene.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoreanWarzone.Scene
+{
+    public class TitleScene : BaseScene
+    {
+        public override void Render()
+        {
+            Console.WriteLine("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+            Console.WriteLine("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+            Console.WriteLine("&&&&&&&&&&&&&                    &&&&&&&&&&&&&&&");
+            Console.WriteLine("&&&&&&&&&&&&&                    &&&&&&&&&&&&&&&");
+            Console.WriteLine("&&&&&&&&&&&&&      Seoul DMZ     &&&&&&&&&&&&&&&");
+            Console.WriteLine("&&&&&&&&&&&&&                    &&&&&&&&&&&&&&&");
+            Console.WriteLine("&&&&&&&&&&&&&                    &&&&&&&&&&&&&&&");
+            Console.WriteLine("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+            Console.WriteLine("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
+            Console.WriteLine("Press Any key");
+        }
+        public override void Input()
+        {
+            Console.ReadKey(true);
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public override void Result()
+        {
+            
+        }
+    }
+}
